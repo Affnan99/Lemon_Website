e63b0df54147b23fe41ecaf48087bf2f45f7a49b
Affnan99 <86743028+Affnan99@users.noreply.github.com>
2023-05-29 20:20:59 +0530

    Add files via upload

---
+++ src/routes/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header'
+import HeaderElement from '../components/HeaderElement';
+import Footer from '../components/Footer';
+
+
+const About = () => {
+  return (
+    <div>
+         <HeaderElement />
+         <Footer />
+      <Header />
+      <h1 style={{textAlign: "center", paddingTop: "15%"}}>About</h1>
+    </div>
+    
+  )
+}
+
+export default About
